495d91fb44d20847099f406bb298f5c29f2dc628
Harry <poripori1030@gmail.com>
2018-02-19 07:08:46 +0800

    Connected to Redux

--- App.js
+++ App.js
@@ -1,67 +1,48 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
+import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index'
+
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+
+
 // import placeImage from './src/assets/placeholder.jpg';
 
-export default class App extends React.Component {
-  state = {
-    places: [],
-    selectedPlace: null
+class App extends React.Component {
+
+  componentDidMount() {
+
   }
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      }
-    });
+    this.props.onDeletePlace();
   }
 
   onModalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace();
   }
 
   placeAddedHandler = placeName => {
-      this.setState(prevState => {
-        return {
-          places: prevState.places.concat({
-            key: Math.random(), 
-            name: placeName,
-            image: {
-              uri: "https://i0.wp.com/lovecuteanimals.objects.cdn.dream.io/wp-content/uploads/2016/01/Cute-Netherland-Dwarf-Rabbit.jpg?w=1160"
-            }})
-        };
-      });
+     this.props.onAddPlace(placeName);
   }
 
   placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key;
-        })
-      }
-    });
+    this.props.onSelectPlace(key);
   }
   
   render() {
     return (
       <View style={styles.container}>
           <PlaceDetail
-           selectedPlace={this.state.selectedPlace} 
+           selectedPlace={this.props.selectedPlace} 
            onItemDeleted={this.placeDeletedHandler} 
            onModalClosed={this.onModalClosedHandler}/>
           <PlaceInput 
           onPlaceAdded={this.placeAddedHandler}/>
           <PlaceList 
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
@@ -78,3 +59,21 @@ const styles = StyleSheet.create({
   }
 
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
