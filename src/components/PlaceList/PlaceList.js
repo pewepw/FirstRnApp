f3df4e6c63de5b4b42c331622f5d3a46fea742b3
Harry <poripori1030@gmail.com>
2018-02-18 23:48:16 +0800

    Update

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView} from 'react-native';
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (<ListItem key={i} placeName={place}/>));
+    const placesOutput = props.places.map((place, i) => (
+        <ListItem key={i} 
+        placeName={place} 
+        onItemPressed={() => props.onItemDeleted(i)}/>
+    ));
 
     return (
-        <View style={styles.listContainer}>
+        <ScrollView style={styles.listContainer}>
         {placesOutput}
-      </View>
+      </ScrollView>
     );
 };
 
